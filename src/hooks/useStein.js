4afae11cfeb7dc7b23f09673e717f2a559ba350e
Yosua Risandy <jojo.almostover@gmail.com>
2022-09-20 17:52:58 +0700

    feat(hooks): add stein hooks and env

---
+++ src/hooks/useStein.js
@@ -0,0 +1,54 @@
+import React from "react";
+import SteinStore from "stein-js-client";
+
+const store = new SteinStore(process.env.REACT_APP_STEIN_STORE);
+
+const useStein = (sheetName = 'Sheet1') => {
+    const [loading, setLoading] = React.useState(false);
+    const [error, setError] = React.useState('');
+    const [data, setData]= React.useState([]);
+
+    const getList = (options) => {
+        setLoading(true);
+        setError('');
+        store.read(sheetName, options).then(result => {
+            setData(result)
+        }).catch(e => {
+            setError(e.message);
+        }).finally(() => {
+            setLoading(false);
+        });
+    }
+
+    const onEdit = (options) => {
+        setLoading(true);
+        setError('');
+        store.edit(sheetName, ...options).then(result => {
+            setData(result)
+        }).catch(e =>{
+            setError(e.message);
+        }).finally(()=>{
+            setLoading(false);
+        });
+    }
+
+    const onDelete = (options) => {
+        setLoading(true);
+        setError('');
+        store.delete(sheetName, ...options).then(result => {
+            setData(result)
+        }).catch(e =>{
+            setError(e.message);
+        }).finally(()=>{
+            setLoading(false);
+        });
+    }
+
+    React.useEffect(() =>{
+        getList()
+    }
+    ,[]);
+
+    return { loading, data, error, getList, onEdit, onDelete  };
+}
+export default useStein
