67aa2cfc538d1efe9ca544f63880c8189c25982d
Yosua Risandy <jojo.almostover@gmail.com>
2022-09-20 16:20:29 +0700

    feat(stein): add stein client and testing crud using stein client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SteinStore from "stein-js-client";
 
 function App() {
+  const store = new SteinStore("https://api.steinhq.com/v1/storages/632976687bccea08c119169b");
+
+  React.useEffect(() => {
+  store.read("Sheet1", { offset: 2 }).then(data => {
+    console.log(data);
+  });
+  });
+
+  const handleAdd = () => {
+    store.append("Sheet1", [
+          {
+            title: "add",
+            author: "Me!",
+            content: "A brief summary",
+            link: "blog.me.com/awesome-article"
+          }
+        ])
+        .then(res => {
+          console.log(res);
+        });
+  }
+
+  const handleUpdate = () => {
+      store
+          .edit("Sheet1", {
+              search: { author: "Me!" },
+              set: { title: "Currently Unavailable" }
+          })
+          .then(res => {
+              console.log(res);
+          });
+  }
+
+  const handleDelete = () => {
+      store
+          .delete("Sheet1", {
+              search: { author: "Me!" }
+          })
+          .then(res => {
+              console.log(res);
+          });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleAdd}>add</button>
+      <button onClick={handleUpdate}>update</button>
+      <button onClick={handleDelete}>delete</button>
     </div>
   );
 }
